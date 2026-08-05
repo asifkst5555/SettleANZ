c7c741b5b0e21c9e51d08c3221fa58c5b2156db9
asifkst5555 <asifkst5555@gmail.com>
2026-08-05 10:48:13 +0600

    feat(favicon): use public/media/favicon master ICO and PNG sources with standardized head tags

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -41,13 +41,10 @@
     <meta http-equiv="Expires" content="0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $metaTitle ?? 'Admin | SettleANZ' }}</title>
-    <link rel="icon" type="image/x-icon" href="/favicon.ico?v={{ $favicon_ver }}">
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico?v={{ $favicon_ver }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v={{ $favicon_ver }}">
+    <link rel="icon" href="/favicon.ico?v={{ $favicon_ver }}" sizes="any">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v={{ $favicon_ver }}">
     <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png?v={{ $favicon_ver }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png?v={{ $favicon_ver }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v={{ $favicon_ver }}">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png?v={{ $favicon_ver }}">
     <link rel="manifest" href="/site.webmanifest?v={{ $favicon_ver }}">
     <meta name="theme-color" content="#0f8b8d">
     <link rel="preconnect" href="https://fonts.googleapis.com">
--- resources/views/landing/download-error.blade.php
+++ resources/views/landing/download-error.blade.php
@@ -7,13 +7,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $metaTitle }}</title>
-    <link rel="icon" type="image/x-icon" href="/favicon.ico?v={{ $faviconVer }}">
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico?v={{ $faviconVer }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v={{ $faviconVer }}">
+    <link rel="icon" href="/favicon.ico?v={{ $faviconVer }}" sizes="any">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v={{ $faviconVer }}">
     <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png?v={{ $faviconVer }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png?v={{ $faviconVer }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v={{ $faviconVer }}">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png?v={{ $faviconVer }}">
     <link rel="manifest" href="/site.webmanifest?v={{ $faviconVer }}">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
